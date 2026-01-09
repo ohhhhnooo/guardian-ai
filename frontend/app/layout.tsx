cbd6e9c31cfdbedd27198835b42d1c282d1f8fb5
Viktoria <v.p.persuk@niuitmo.ru>
2026-01-09 17:34:48 +0300

    chore(front): change in header

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { ReactNode } from "react";
 import Providers from "./providers";
+import 'leaflet/dist/leaflet.css';
 
 export const metadata: Metadata = {
   title: "Guardian AI – Drone Flight Weather Assistant",
@@ -17,5 +18,3 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     </html>
   );
 }
-
-
